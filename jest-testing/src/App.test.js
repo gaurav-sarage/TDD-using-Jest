889aeed661bb3bf84bd0754edf403b45e376b5ce
Gaurav <gauravsarage.1902@gmail.com>
2023-07-18 23:52:50 +0530

    added test function to test the log in/out of the app

--- jest-testing/src/App.test.js
+++ jest-testing/src/App.test.js
@@ -1,1 +1,27 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders Logged out", () => {
+    render(<App />);
+
+    expect(screen.getByRole("heading").textContent).toMatch("Logged out")
+  });
+
+  it("renders logged in after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    
+    const button = screen.getByRole("button", {
+      name: "Log In"
+    });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch("Logged in");
+  });
+});
+
