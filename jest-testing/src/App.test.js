d7407163157d337212101c3eee764fd62460c016
Gaurav <gauravsarage.1902@gmail.com>
2023-07-18 23:32:45 +0530

    uses render & screen to test what is displayed on screen & describe groups all tests for each component together for readibility

--- jest-testing/src/App.test.js
+++ jest-testing/src/App.test.js
@@ -1,1 +1,10 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+  test("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch("My App");
+  });
+});
